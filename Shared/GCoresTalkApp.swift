229491b7b5f7b8e38ed691d18bfecd4930703391
catbaron0 <catbaron@live.cn>
2021-12-13 00:54:29 +0900

    add command of shift+cmd+N to post new talk

--- Shared/GCoresTalkApp.swift
+++ Shared/GCoresTalkApp.swift
@@ -11,12 +11,21 @@ import SwiftUI
 struct GCoresTalkApp: App {
     let persistenceController = PersistenceController.shared
     @StateObject var gtalk = GCoresTalk()
-
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
                 .environment(\.managedObjectContext, persistenceController.container.viewContext).environmentObject(gtalk)
         }.windowStyle(HiddenTitleBarWindowStyle())
-
+            .commands {
+                CommandMenu("First menu") {
+                    Button("New Talk") {
+                        let status = gtalk.statusForScene[gtalk.selectedTalkSceneType]!.last!
+                        let newStatus = ViewStatus(id: UUID().uuidString, sceneType: .newWindow, statusType: .newTalk, title: "新 Talk", icon: "pencil.and.outline")
+                        newNSWindow(view: NewTalkView(status: newStatus, gtalk: gtalk, topic: status.topic))
+                    }.keyboardShortcut("N", modifiers: [.command, .shift])
+                }
+            }
+        
     }
 }
